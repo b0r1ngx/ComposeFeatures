c4a3deccd870ef516667e39b8d3d5db28322ead4
Ivanov Kirill A <perkeboring@gmail.com>
2024-09-27 12:58:37 +0300

    Run application in full screen size

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,22 +1,20 @@
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.b0r1ngx.composefeatures.common.App
 
 
 fun main() = application {
+    // rememberWindowState(size = DpSize(1280.dp, 720.dp))
+    val windowState = rememberWindowState(placement = WindowPlacement.Fullscreen)
+
     Window(
         title = "Compose Features",
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState(
-            size = DpSize(
-                1280.dp,
-                720.dp
-            )
-        ),
-//        onKeyEvent =
+        state = windowState,
     ) {
         App()
     }
